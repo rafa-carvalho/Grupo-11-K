1bae15c32d4e664afdb2bd33ad93b1699aa862ea
rafa-carvalho <79550207+rafa-carvalho@users.noreply.github.com>
2021-11-20 12:52:42 -0300

    Prototipo

--- prototipo.c
+++ prototipo.c
@@ -2,13 +2,68 @@
 #include <string.h>
 #include <stdlib.h>
 #define max 256
+#define max_pontos 500
+
+/*criacao a pilha*/
+struct pilha {
+    int itens[max_pontos];
+    int topo;
+};
+
+void iniciar_pilha(struct pilha *p)
+{
+    p->topo = -1;
+}
+
+int pilha_vazia(struct pilha *p)
+{
+    if(p->topo == -1)
+        return 1;
+    else
+        return 0;
+}
+
+int pilha_cheia(struct pilha *p)
+{
+    if(p->topo == max_pontos-1)
+        return 1;
+    else
+        return 0;
+}
+
+void checkin(struct pilha *p, int check)
+{
+    if(pilha_cheia(p) == 1)
+        printf("\nLimite de pontos atingido\n");
+    else {
+        p->topo++;
+        p->itens[p->topo] = check;
+    }
+}
+
+int gastar(struct pilha *p)
+{
+    int aux;
+    if(pilha_vazia(p) == 1)
+        printf("\nVoce não possui nenhum ponto\n");
+    else {
+        p->topo--;
+        return aux;
+    }
+}
+
+int pontos_total(struct pilha *p)
+{
+    return p->topo;
+}
+/*fim*/
 
 struct usuario {
     char email[max];
     char senha[max];
     char nome[max];
     char empresa[max];
-    int frequencia;
+    char plano[max];
 };
 
 char validacao_login(char email[max], char senha[max])
@@ -28,36 +83,33 @@ char validacao_login(char email[max], char senha[max])
     return checagem;
 }
 
-void ranking_empresas(char empresa_user[max])
+void perfil(char nome_user[max], struct pilha *p)
 {
-    FILE *arq;
-    char empresa[max];
-    int i = 1;
-    arq = fopen("ranking.txt", "r");
-    while(fgets(empresa, max, arq) != NULL) {
-        if(strcmp(empresa, empresa_user) == 0) {
-            printf("%d lugar\n", i);
-            break;
-        }
-        i++;
-    }
-    fclose(arq);
-}
-
-void perfil(char nome_user[max], int frequencia)
-{
-    int opcao_perfil;
+    int opcao_perfil, opcao_compra;
     while (opcao_perfil != 4) {
         printf("\nPerfil: %s", nome_user);
         printf("1-Frequencia\n2-Beneficios\n3-Conquistas\n4-Sair\n>> ");
         scanf("%d", &opcao_perfil);
         switch (opcao_perfil) {
         case 1:
-            printf("%d pontos\n", frequencia);
+            printf("\n%d pontos\n", pontos_total(p));
             break;
         case 2:
-            printf("%d pontos\n", frequencia);
-            printf("*Pagina com produtos* EX:\n(1) CUPOM 10%% XXX - xxx pontos\n>> ");
+            printf("\n%d pontos\n", pontos_total(p));
+            printf("*Pagina com produtos* EX:\n(1) CUPOM 10%% XXX - 10 pontos\n>> ");
+            scanf("%d", &opcao_compra);
+            switch (opcao_compra) {
+            case 1:
+                if(pontos_total(p) >= 10) {
+                    for(int i = 0; i < 10; i++)
+                        gastar(p);
+                }
+                else
+                    printf("Voce nao tem pontos suficiente\n");
+                break;
+            default:
+                break;
+            }
             break;
         case 3:
             /*pagina de trofeus e conquistas*/
@@ -93,6 +145,10 @@ void buscar()
 
 int main(int argc, char const *argv[])
 {
+    /*alocando a pilha*/
+    struct pilha *p = (struct pilha*)malloc(sizeof(struct pilha));
+    /*fim*/
+
     struct usuario user;
     /*cadastro feito pela propria empresa*/
     printf("\nLOGIN\n");
@@ -111,7 +167,7 @@ int main(int argc, char const *argv[])
                 fgets(senha_atual, max, arq);
                 fgets(user.nome, max, arq);
                 fgets(user.empresa, max, arq);
-                fscanf(arq, "%d", &user.frequencia);
+                fgets(user.plano, max, arq);
             }
         }
         fclose(arq);
@@ -120,7 +176,7 @@ int main(int argc, char const *argv[])
         char fim;
         while (opcao != 4) {
             printf("\nGYMTECH\n");
-            printf("1-Pesquisar\n2-Meus Grupos\n3-Meu Perfil\n4-Ranking Empresa\n5-Sair\n>> ");
+            printf("1-Pesquisar\n2-Meus Grupos\n3-Meu Perfil\n4-Check-In\n5-Sair\n>> ");
             scanf("%d", &opcao);
             switch (opcao) {
             case 1:
@@ -130,11 +186,29 @@ int main(int argc, char const *argv[])
                 /*grupos*/
                 break;
             case 3:
-                perfil(user.nome, user.frequencia);
+                perfil(user.nome, p);
                 break;
             case 4:
-                printf("Ranking: %s", user.empresa);
-                ranking_empresas(user.empresa);
+                if(strcmp(user.plano, "Basic\n") == 0) {
+                    checkin(p, 1);
+                }
+                else if(strcmp(user.plano, "Silver\n") == 0) {
+                    for(int i = 0; i < 2; i++)
+                        checkin(p, 1);
+                }
+                else if(strcmp(user.plano, "Gold\n") == 0) {
+                    for(int i = 0; i < 3; i++)
+                        checkin(p, 1);
+                }
+                else if(strcmp(user.plano, "Platinum\n") == 0) {
+                    for(int i = 0; i < 4; i++)
+                        checkin(p, 1);
+                }
+                else if(strcmp(user.plano, "Black\n") == 0) {
+                    for(int i = 0; i < 5; i++)
+                        checkin(p, 1);
+                }
+                printf("Check-in confirmado!\n");
                 break;
             case 5:
                 printf("Voce tem certeza? (S) (N)\n>> ");
@@ -151,5 +225,7 @@ int main(int argc, char const *argv[])
     }
     else
         printf("Login invalido\n");
+
+    free(p);
     return 0;
 }
